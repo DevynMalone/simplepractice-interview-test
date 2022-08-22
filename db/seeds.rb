5ef60cd9d1c70e125fbe5b7b9f9502a3084d7a88
“DevynMalone” <Dee_malone24@yahoo.com>
2022-08-22 10:20:57 -0700

    updating dotors

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,16 @@
 #   - Each appointment should be 50 minutes in duration
 
 
-Doctors.create(name:'Doctor 1')
+Doctors.create(name: 'Doctor 1')
+Doctors.create(name: 'Doctor 2')
+Doctors.create(name: 'Doctor 3')
+Doctors.create(name: 'Doctor 4')
+Doctors.create(name: 'Doctor 5')
+Doctors.create(name: 'Doctor 6')
+Doctors.create(name: 'Doctor 7')
+Doctors.create(name: 'Doctor 8')
+Doctors.create(name: 'Doctor 9')
+Doctors.create(name: 'Doctor 10')
+
+
+Doctor.where(name: 'Doctor 1')
